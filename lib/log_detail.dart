bb87b4dbae6f28ea52f361177ce0daf9a3f0bbaf
h.kuwabara <h.kuwabara1103@gmail.com>
2022-08-08 23:10:54 +0900

    距離の入力フォームを作成

--- lib/log_detail.dart
+++ lib/log_detail.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LogDetail extends StatelessWidget {
   const LogDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('記録詳細'),
+    OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(5.0),
+      borderSide: const BorderSide(color: Color(0xFFd3d3d3)),
+    );
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('記録詳細'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 4, bottom: 4),
+                child: Text("距離",
+                    style: TextStyle(
+                      color: Color(0xFF758492),
+                    )),
+              ),
+              SizedBox(
+                width: 100,
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFf0f8ff),
+                      focusedBorder: outlineInputBorder,
+                      enabledBorder: outlineInputBorder,
+                      hintText: '0',
+                      suffixText: 'm'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: const Text('記録詳細'),
     );
   }
 }
